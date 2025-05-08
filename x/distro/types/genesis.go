d736c784e017fdb26f9b68669f3af42f63d7def8
cbrotherson <cbrotherson@nerdunited.com>
2025-05-08 15:39:48 -0600

    Remove unnecessary default genesis code

--- x/distro/types/genesis.go
+++ x/distro/types/genesis.go
@@ -1,35 +1,13 @@
 package types
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-)
-
 // DefaultIndex is the default global index
 const DefaultIndex uint64 = 1
 
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
-	dailyDistributionTotals := make(map[string]uint64)
-	data, err := os.ReadFile("app/upgrades/v2/daily_distribution_totals.json")
-	if err == nil {
-		var jsonDailyDistributionTotals []struct {
-			Date   string  `json:"date"`
-			Amount float64 `json:"amount"`
-		}
-
-		if err := json.Unmarshal(data, &jsonDailyDistributionTotals); err != nil {
-			panic(fmt.Errorf("failed to unmarshal daily distribution totals: %w", err))
-		}
-
-		for _, dailyDistributionTotal := range jsonDailyDistributionTotals {
-			dailyDistributionTotals[dailyDistributionTotal.Date] = uint64(dailyDistributionTotal.Amount)
-		}
-	}
-
 	return &GenesisState{
 		// this line is used by starport scaffolding # genesis/types/default
+
 		Params: DefaultParams(),
 	}
 }
