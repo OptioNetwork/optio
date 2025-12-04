a4d7f0116600ab4c4c4b9f844bd03d9d3e19ca72
Christian Brotherson <108023689+cbrotherson@users.noreply.github.com>
2025-12-04 12:09:58 -0700

    The ante handler only needs to check that a MsgUndelegate does not take the total delegations below the locked amount

--- x/lockup/ante/ante_handler.go
+++ x/lockup/ante/ante_handler.go
@@ -4,25 +4,21 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
 	"github.com/OptioNetwork/optio/x/lockup/types"
-	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 	authztypes "github.com/cosmos/cosmos-sdk/x/authz"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
-
-	"cosmossdk.io/x/feegrant"
-
-	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // NewLockedBalanceDecorator checks that the sender has sufficient unlocked balance for msgs that spend funds.
 type LockedBalanceDecorator struct {
 	accountKeeper ante.AccountKeeper
 	bankKeeper    bankkeeper.Keeper
+	stakingKeeper stakingkeeper.Keeper
 }
 
 func NewLockedBalanceDecorator(accountKeeper ante.AccountKeeper, bankKeeper bankkeeper.Keeper) LockedBalanceDecorator {
@@ -33,113 +29,102 @@ func NewLockedBalanceDecorator(accountKeeper ante.AccountKeeper, bankKeeper bank
 }
 
 func (lbd LockedBalanceDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	for _, msg := range tx.GetMsgs() {
-		switch msg.(type) {
-		case *banktypes.MsgSend:
+	msgs := tx.GetMsgs()
 
-			sendMsg := msg.(*banktypes.MsgSend)
-			fromAddr, err := sdk.AccAddressFromBech32(sendMsg.FromAddress)
-			if err != nil {
-				return ctx, err
-			}
-			acc := lbd.accountKeeper.GetAccount(ctx, fromAddr)
-			if ltsAcc, ok := acc.(*types.Account); ok {
-				// Calculate locked balance
-				locked := math.NewInt(0)
-				blockTime := ctx.BlockTime()
-				for unlockDate, lockup := range ltsAcc.Lockups {
-					if types.IsLocked(blockTime, unlockDate) {
-						locked = locked.Add(lockup.Coin.Amount)
-					}
-				}
-				// Get total balance
-				totalBalance := lbd.bankKeeper.GetBalance(ctx, fromAddr, sendMsg.Amount[0].Denom).Amount
-				available := totalBalance.Sub(locked)
-				if available.LT(sendMsg.Amount[0].Amount) {
-					return ctx, errorsmod.Wrapf(errortypes.ErrInsufficientFunds, "insufficient unlocked balance: available %s, required %s", available, sendMsg.Amount[0].Amount)
-				}
-			}
-
-		case *banktypes.MsgMultiSend:
-
-			multiSendMsg := msg.(*banktypes.MsgMultiSend)
-			// Check each input
-			for _, input := range multiSendMsg.Inputs {
-				fromAddr, err := sdk.AccAddressFromBech32(input.Address)
-				if err != nil {
-					return ctx, err
-				}
-				acc := lbd.accountKeeper.GetAccount(ctx, fromAddr)
-				if ltsAcc, ok := acc.(*types.Account); ok {
-					// Calculate locked balance
-					locked := math.NewInt(0)
-					blockTime := ctx.BlockTime()
-					for unlockDate, lockup := range ltsAcc.Lockups {
-						if types.IsLocked(blockTime, unlockDate) {
-							locked = locked.Add(lockup.Coin.Amount)
-						}
-					}
-					// Get total balance for each coin in input
-					for _, coin := range input.Coins {
-						totalBalance := lbd.bankKeeper.GetBalance(ctx, fromAddr, coin.Denom).Amount
-						available := totalBalance.Sub(locked)
-						if available.LT(coin.Amount) {
-							return ctx, errorsmod.Wrapf(errortypes.ErrInsufficientFunds, "insufficient unlocked balance: available %s, required %s", available, coin.Amount)
-						}
-					}
-				}
-			}
-
-		case *authztypes.MsgGrant: // Is this needed? This doesn't actually move funds, just grants permission.
-
-			msgGrant := msg.(*authztypes.MsgGrant)
-			// Check each input
-			fromAddr, err := sdk.AccAddressFromBech32(msgGrant.Granter)
-			if err != nil {
-				return ctx, err
-			}
-			acc := lbd.accountKeeper.GetAccount(ctx, fromAddr)
-			if ltsAcc, ok := acc.(*types.Account); ok {
-				// Calculate locked balance
-				locked := math.NewInt(0)
-				blockTime := ctx.BlockTime()
-				for unlockDate, lockup := range ltsAcc.Lockups {
-					if types.IsLocked(blockTime, unlockDate) {
-						locked = locked.Add(lockup.Coin.Amount)
-					}
-				}
-				// Get total balance for each coin in input
-				if msgGrant.Grant.Authorization.GetTypeUrl() != sdk.MsgTypeURL(&banktypes.SendAuthorization{}) {
-					authorization, err := msgGrant.Grant.GetAuthorization()
-					if err != nil {
-						return ctx, err
-					}
-					sendAuth, ok := authorization.(*banktypes.SendAuthorization)
-					if ok {
-						for _, coin := range sendAuth.SpendLimit {
-							totalBalance := lbd.bankKeeper.GetBalance(ctx, fromAddr, coin.Denom).Amount
-							available := totalBalance.Sub(locked)
-							if available.LT(coin.Amount) {
-								return ctx, errorsmod.Wrapf(errortypes.ErrInsufficientFunds, "insufficient unlocked balance: available %s, required %s", available, coin.Amount)
-							}
-						}
-					}
-				}
-			}
-
-		// TODO: flesh out these msg types to enforce locked balance checks
-		case *authztypes.MsgExec:
-		case *govtypes.MsgDeposit:
-		case *distributiontypes.MsgFundCommunityPool:
-		case *feegrant.MsgGrantAllowance:
-		case *ibctransfertypes.MsgTransfer:
-		// case *types.MsgSendAndLock:
-		// case *types.MsgMultiSendAndLock:
-
-		default:
-			// skip other messages
-		}
+	err = handleMsgs(ctx, msgs, lbd)
+	if err != nil {
+		return ctx, err
 	}
 
 	return next(ctx, tx, simulate)
+}
+
+func handleMsgs(ctx sdk.Context, msgs []sdk.Msg, lbd LockedBalanceDecorator) error {
+	for _, msg := range msgs {
+		switch msg.(type) {
+		case *stakingtypes.MsgUndelegate:
+
+			err := handleMsgUndelegate(ctx, msg, lbd)
+			if err != nil {
+				return err
+			}
+
+		case *authztypes.MsgExec:
+
+			err := handleMsgExec(ctx, msg, lbd)
+			if err != nil {
+				return err
+			}
+
+		default:
+			return nil
+		}
+	}
+
+	return nil
+}
+
+// handleMsgUndelegate checks if the undelegation would cause the delegator's total delegated amount to be less than their locked amount.
+func handleMsgUndelegate(ctx sdk.Context, msg sdk.Msg, lbd LockedBalanceDecorator) error {
+	msgUndelegate := msg.(*stakingtypes.MsgUndelegate)
+	fromAddr, err := sdk.AccAddressFromBech32(msgUndelegate.DelegatorAddress)
+	if err != nil {
+		return err
+	}
+
+	acc := lbd.accountKeeper.GetAccount(ctx, fromAddr)
+	if ltsAcc, ok := acc.(*types.Account); ok {
+		totalLocked := math.NewInt(0)
+		blockTime := ctx.BlockTime()
+		for unlockDate, lockup := range ltsAcc.Lockups {
+			if types.IsLocked(blockTime, unlockDate) {
+				totalLocked = totalLocked.Add(lockup.Coin.Amount)
+			}
+		}
+
+		delegations, err := lbd.stakingKeeper.GetDelegatorDelegations(ctx, fromAddr, 1000)
+		if err != nil {
+			return err
+		}
+
+		totalDelegated := math.NewInt(0)
+		for _, delegation := range delegations {
+			totalDelegated = totalDelegated.Add(delegation.Shares.TruncateInt())
+		}
+
+		delegatedAfterUndelegate := totalDelegated.Sub(msgUndelegate.Amount.Amount)
+
+		if delegatedAfterUndelegate.LT(totalLocked) {
+			return errorsmod.Wrapf(
+				types.ErrInsufficientDelegations,
+				"undelegation would cause new delegated amount to be less than the locked amount: %s < %s",
+				delegatedAfterUndelegate.String(),
+				totalLocked.String(),
+			)
+		}
+	}
+
+	return nil
+}
+
+// handleMsgExec checks each inner message of MsgExec for locked balance constraints.
+func handleMsgExec(ctx sdk.Context, msg sdk.Msg, lbd LockedBalanceDecorator) error {
+	msgGrant := msg.(*authztypes.MsgExec)
+	var sdkMsgs []sdk.Msg
+	for _, innerMsg := range msgGrant.Msgs {
+		var sdkMsg sdk.Msg
+		registry := codectypes.NewInterfaceRegistry()
+		err := registry.UnpackAny(innerMsg, &sdkMsg)
+		if err != nil {
+			return err
+		}
+		sdkMsgs = append(sdkMsgs, sdkMsg)
+	}
+
+	err := handleMsgs(ctx, sdkMsgs, lbd)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- x/lockup/types/errors.go
+++ x/lockup/types/errors.go
@@ -8,6 +8,7 @@ import (
 
 // x/lockup module sentinel errors
 var (
-	ErrInvalidSigner  = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrInvalidAccount = sdkerrors.Register(ModuleName, 1101, "invalid account type for lockup")
+	ErrInvalidSigner           = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidAccount          = sdkerrors.Register(ModuleName, 1101, "invalid account type for lockup")
+	ErrInsufficientDelegations = sdkerrors.Register(ModuleName, 1102, "insufficient delegations amount to cover locked amount")
 )
